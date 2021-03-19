5a3d25f3ac994cca5b72a454b23439a2c3f34a26
thanhdo95 <58335570+thanhdo95@users.noreply.github.com>
2021-03-19 19:41:42 +0700

    PracticalTest

---
+++ UWP/Exam/Exam/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Exam
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        string path;
+        SQLite.Net.SQLiteConnection conn;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "database.sqlite"); ;//tên file,phía trc là đường dẫn
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);//tạo ra 1 form sqlite theo window
+            conn.CreateTable<PhoneNumber>();//tên bảng customer
+        }
+        public class PhoneNumber
+        {
+            [PrimaryKey]
+            public string Phone { get; set; }
+            public string Name { get; set; }
+          
+            //prop ra nhanh+tab+tab
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var phone = conn.Insert(new PhoneNumber()
+                {
+                    Name = textBoxName.Text,
+                    Phone = textBoxPhone.Text,
+                   // Phone = (Int32.Parse(textBoxPhone.Text)),
+                });
+            }
+            catch
+            {
+                Console.WriteLine("Số điện thoại bị đã có");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var query = conn.Table<PhoneNumber>();
+         
+            string name = "";
+            string Phone = "";
+            foreach (var message in query)
+            {
+               
+                name = name + " " + message.Name;
+                Phone = Phone+ " " + message.Phone;
+            }
+            textBoxShow.Text = "\nName: " + name + "\nPhone: " + Phone;
+        }
+    }
+}
---
+++ UWP/Exam/Exam/Search.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using static Exam.MainPage;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Exam
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Search : Page
+    {
+        string path;
+        SQLite.Net.SQLiteConnection conn;
+        public Search()
+        {
+            this.InitializeComponent();
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "database.sqlite"); //tên file,phía trc là đường dẫn
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var query = conn.Table<PhoneNumber>();
+                string name = "";
+                string phone = "";
+                foreach (var message in query)
+                {
+
+                    name = name + " " + message.Name;
+                    phone = phone + " " + message.Phone;
+                }
+                if (String.Compare(textBoxName1.Text, name, true) == 0 && String.Compare(textBoxPhone1.Text, phone, true) == 0)
+                {
+                    TextBlockShow.Text = "\nName: " + name + "\nPhoneNumber: " + phone;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
